2a0ef049c5d143bff8bf51b9b08dd558ebcfac25
Anthony Gadaleta <agad495@gmail.com>
2022-06-08 06:53:02 -0400

    MLB Scraping Revision
    Updated MLB game scraper to correctly pull from API for only each team's next game.

--- mlb_scrapers/dksb_mlb.py
+++ mlb_scrapers/dksb_mlb.py
@@ -84,60 +84,152 @@ class ScrapeDKBases():
         games : dictionary containing teams, moneylines, totals, runlines and opponents.
 
         """
-        dk_api = requests.get("https://sportsbook-us-nj.draftkings.com//sites/US-NJ-SB/api/v4/eventgroups/88670847?includePromotions=true&format=json").json()
-        dk_markets = dk_api['eventGroup']['offerCategories'][0]['offerSubcategoryDescriptors'][0]['offerSubcategory']['offers']
-        
         games = {}
-        for i in dk_markets:
-            away_team = self.team_cleanup(i[2]['outcomes'][0]['label'])
-            home_team = self.team_cleanup(i[2]['outcomes'][1]['label'])
+        market_ids = {'Game':493, '1st 5':729, 'TT':724}
+        for cat in market_ids: # These are offerCategoryIds for game, 1H, 1Q and TT's
+            dk_api = requests.get(f"https://sportsbook.draftkings.com//sites/US-NJ-SB/api/v4/eventgroups/88670847/categories/{market_ids[cat]}?format=json").json()
+            if 'offerCategories' not in dk_api['eventGroup']:
+                continue
+            for i in dk_api['eventGroup']['offerCategories']:
+                if 'offerSubcategoryDescriptors' in i:
+                    dk_markets = i['offerSubcategoryDescriptors']
             
-            if away_team not in games: 
-                games[away_team] = {}
+                    market_names = ['Game', 'Alternate Spread', 'Alternate Total', 'Team Totals',
+                                    'Alternate Point Spread', 'Alternate Total Points',
+                                    'Alternate Run Line', 'Alternate Total Runs',
+                                    '1st 5 Innings', '1st 5 Innings - Moneyline',
+                                    'Team Total Runs', 'Alt Total']
+                    subcategoryIds = []# Need subcategoryIds first
+                    for i in dk_markets:
+                        if i['name'] in market_names:
+                            subcategoryIds.append(i['subcategoryId'])
+                
+            game_ids = {}
+            for i in dk_api['eventGroup']['events']:
+                if i['eventStatus']['state'] != 'NOT_STARTED':
+                    continue
+                next_id = False
+                if i['eventGroupName'] == 'MLB':
+                    for x in game_ids:
+                        if (game_ids[x]['home'] == i['teamName2']) & (game_ids[x]['away'] == i['teamName1']):
+                            next_id = True
+                if not next_id:
+                    game_ids[i['providerEventId']] = {'home':i['teamName2'], 'away':i['teamName1'],
+                                                      'date': i['startDate']}
+                            
+            for ids in subcategoryIds:
+                dk_api = requests.get(f"https://sportsbook.draftkings.com//sites/US-NJ-SB/api/v4/eventgroups/88670847/categories/{market_ids[cat]}/subcategories/{ids}?format=json").json()
                 try:
-                    games[away_team]['moneyline'] = i[2]['outcomes'][0]['oddsDecimal']
+                    dk_api['eventGroup']['offerCategories']
                 except:
-                    pass
-                try:
-                    games[away_team]['runline'] = [i[0]['outcomes'][0]['line'],
-                                                   i[0]['outcomes'][0]['oddsDecimal']]
-                except:
-                    pass
-                try:
-                    games[away_team]['over'] = [i[1]['outcomes'][0]['line'],
-                                                i[1]['outcomes'][0]['oddsDecimal']]
-                except:
-                    pass
-                try:
-                    games[away_team]['under'] = [i[1]['outcomes'][1]['line'],
-                                                 i[1]['outcomes'][1]['oddsDecimal']]
-                except:
-                    pass            
-            
-                games[away_team]['opponent'] = home_team
+                    continue
+                for i in dk_api['eventGroup']['offerCategories']:
+                    if 'offerSubcategoryDescriptors' in i:
+                        dk_markets = i['offerSubcategoryDescriptors']
+                
+                for i in dk_markets:
+                    if 'offerSubcategory' in i:
+                        for j in i['offerSubcategory']['offers']:
+                            for k in j:
+                                if 'providerEventId' in k:
+                                    event = k['providerEventId']
+                                else:
+                                    continue
+                                
+                                try:
+                                    away_team = self.team_cleanup(game_ids[event]['away'])
+                                    home_team = self.team_cleanup(game_ids[event]['home'])
+                                except:
+                                    continue
+                                    
+                                if away_team not in games:
+                                    games[away_team] = {home_team: {'location': 0}}
+                                elif home_team not in games[away_team]:
+                                    games[away_team][home_team] = {'location': 0}
+                                if home_team not in games:
+                                    games[home_team] = {away_team: {'location': 1}}
+                                elif away_team not in games[home_team]:
+                                    games[home_team][away_team] = {'location': 1}
+                                                                        
+                                try: # If market has no label, go to the next one (it is likely closed/unavailable)
+                                    market = k['label']
+                                    if '2nd Half' in market:
+                                        continue
+                                except:
+                                    continue
+                                
+                                periods = ['Game', '1st 5 Innings']
+                                                                    
+                                if ('Spread' in market) | ('Run Line' in market):
+                                    if 'spread' not in games[away_team][home_team]:
+                                        games[away_team][home_team]['spread'] = {i: {} for i in periods}
+                                        games[home_team][away_team]['spread'] = {i: {} for i in periods}
+                                    
+                                    for side in k['outcomes']:
+                                        try:
+                                            if self.name_cleanup(side['label'], sport, 'DK') == away_team:
+                                                games[away_team][home_team]['spread'][cat][side['line']] = side['oddsDecimal']
+                                            elif  self.name_cleanup(side['label'], sport, 'DK') == home_team:
+                                                games[home_team][away_team]['spread'][cat][side['line']] = side['oddsDecimal']
+                                        except:
+                                            pass
+                                        
+                                elif 'Team Total' in market:
+                                    if cat == 'TT': # I wish this wasn't necessary
+                                        category = 'Game'
+                                    else:
+                                        category = cat
+                                        
+                                    if 'team total' not in games[away_team][home_team]:
+                                        games[away_team][home_team]['team total'] = {i: {'over':{}, 'under':{}} for i in periods}
+                                        games[home_team][away_team]['team total'] = {i: {'over':{}, 'under':{}} for i in periods}
+                                    
+                                    for side in k['outcomes']:
+                                        try:
+                                            if 'Touchdowns' in market:
+                                                continue # We don't want team total touchdowns
+                                            if side['label'] == 'Over':
+                                                if self.name_cleanup(re.sub(':.+','', market), sport, 'DK') == away_team:
+                                                    games[away_team][home_team]['team total'][category]['over'][side['line']] = side['oddsDecimal']
+                                                if self.name_cleanup(re.sub(':.+','', market), sport, 'DK') == home_team:                                                    
+                                                    games[home_team][away_team]['team total'][category]['over'][side['line']] = side['oddsDecimal']
+                                            elif  side['label'] == 'Under':
+                                                if self.name_cleanup(re.sub(':.+','', market), sport, 'DK') == away_team:
+                                                    games[away_team][home_team]['team total'][category]['under'][side['line']] = side['oddsDecimal']
+                                                if self.name_cleanup(re.sub(':.+','', market), sport, 'DK') == home_team:                                                    
+                                                    games[home_team][away_team]['team total'][category]['under'][side['line']] = side['oddsDecimal']
+                                        except Exception as e:
+                                            print(e)
+                                            pass
 
-            if home_team not in games:
-                games[home_team] = {}
-                try:
-                    games[home_team]['moneyline'] = i[2]['outcomes'][1]['oddsDecimal']
-                except:
-                    pass
-                try:
-                    games[home_team]['runline'] = [i[0]['outcomes'][1]['line'],
-                                                   i[0]['outcomes'][1]['oddsDecimal']]
-                except:
-                    pass
-                try:
-                    games[home_team]['over'] = [i[1]['outcomes'][0]['line'],
-                                                i[1]['outcomes'][0]['oddsDecimal']]
-                except:
-                    pass
-                try:
-                    games[home_team]['under'] = [i[1]['outcomes'][1]['line'],
-                                                 i[1]['outcomes'][1]['oddsDecimal']]
-                except:
-                    pass   
-                
-                games[home_team]['opponent'] = away_team
-                
+                                elif 'Total' in market:
+                                    if 'total' not in games[away_team][home_team]:
+                                        games[away_team][home_team]['total'] = {i: {'over':{}, 'under':{}} for i in periods}
+                                        games[home_team][away_team]['total'] = {i: {'over':{}, 'under':{}} for i in periods}
+                                    
+                                    for side in k['outcomes']:
+                                        try:
+                                            if side['label'] == 'Over':
+                                                games[away_team][home_team]['total'][cat]['over'][side['line']] = side['oddsDecimal']
+                                                games[home_team][away_team]['total'][cat]['over'][side['line']] = side['oddsDecimal']
+                                            elif  side['label'] == 'Under':
+                                                games[away_team][home_team]['total'][cat]['under'][side['line']] = side['oddsDecimal']
+                                                games[home_team][away_team]['total'][cat]['under'][side['line']] = side['oddsDecimal']
+                                        except:
+                                            pass
+                                        
+                                elif 'Moneyline' in market:
+                                    if 'moneyline' not in games[away_team][home_team]:
+                                        games[away_team][home_team]['moneyline'] = {}
+                                        games[home_team][away_team]['moneyline'] = {}
+                                    
+                                    for side in k['outcomes']:
+                                        try:
+                                            if self.name_cleanup(side['label'], sport, 'DK') == away_team:
+                                                games[away_team][home_team]['moneyline'][cat] = side['oddsDecimal']
+                                            elif  self.name_cleanup(side['label'], sport, 'DK') == home_team:
+                                                games[home_team][away_team]['moneyline'][cat] = side['oddsDecimal']
+                                        except:
+                                            pass
+                                    
         return games
